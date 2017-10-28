74c8553c886df1700f125a58acc00845cee1765a
ankraft <an.kraft@googlemail.com>
2017-10-28 21:56:46 +0200

    Added example handler to change the to: address

---
+++ handlers/FixAddress.py
@@ -0,0 +1,28 @@
+#
+#	FixAddress.py
+#	Mail handler for replacing the to: field
+#
+
+import mlogging, sys
+from email.header import decode_header
+import MailHandler
+
+
+class FixAddress(MailHandler.MailHandler):
+
+	logger	= None
+	toToFix = '<name@some.address.com>'
+	newTo 	= '<other.name@new.address.com>'
+
+	def isEnabled(self):
+		return True
+
+	def setLogger(self, logger):
+		self.logger = logger
+
+	def handleMessage(self, message, mail, callback):
+		if mail.to[0] == self.toToFix:
+			self.logger.log('FixAddress: Changing to: field for ' + mail.to[0] + ' -> ' + self.newTo)
+			callback.setTo(self.newTo)
+		return None
+
